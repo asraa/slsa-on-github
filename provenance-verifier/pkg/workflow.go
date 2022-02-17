525e63684b4ca7aea01f3d158d4103a589af808d
laurentsimon <laurentsimon@google.com>
2022-02-17 00:11:47 +0000

    unit test top level defaults

--- provenance-verifier/pkg/workflow.go
+++ provenance-verifier/pkg/workflow.go
@@ -11,6 +11,7 @@ import (
 var (
 	errorInvalidGitHubWorkflow = errors.New("invalid GitHub workflow")
 	errorDeclaredEnv           = errors.New("env variables are declared")
+	errorDeclaredDefaults      = errors.New("defaults are declared")
 )
 
 type Workflow struct {
@@ -27,11 +28,40 @@ func WorkflowFromBytes(content []byte) (*Workflow, error) {
 	return &self, nil
 }
 
+// =============== Defaults ================ //
+func (w *Workflow) validateTopLevelDefaults() error {
+	return validateDefaults(w.workflow.Defaults, "top level")
+}
+
+func (w *Workflow) validateJobLevelDefaults(job *actionlint.Job) error {
+	return validateDefaults(job.Defaults, fmt.Sprintf("job %s", getJobIdentity(job)))
+}
+
+func validateDefaults(def *actionlint.Defaults, msg string) error {
+	if def != nil {
+		return fmt.Errorf("%s: %w", msg, errorDeclaredDefaults)
+	}
+	return nil
+}
+
+// =============== Env ================ //
 func (w *Workflow) validateTopLevelEnv() error {
 	return validateEnv(w.workflow.Env, "top level")
 }
 
 func (w *Workflow) validateJobLevelEnv(job *actionlint.Job) error {
+	return validateEnv(job.Env, fmt.Sprintf("job %s", getJobIdentity(job)))
+}
+
+func validateEnv(env *actionlint.Env, msg string) error {
+	if env != nil && len(env.Vars) > 0 {
+		return fmt.Errorf("%s: %w", msg, errorDeclaredEnv)
+	}
+	return nil
+}
+
+// =============== Utility ================ //
+func getJobIdentity(job *actionlint.Job) string {
 	var n string
 	switch {
 	case job.Name != nil:
@@ -41,15 +71,7 @@ func (w *Workflow) validateJobLevelEnv(job *actionlint.Job) error {
 	default:
 		n = "unknown-job"
 	}
-
-	return validateEnv(job.Env, fmt.Sprintf("job %s", n))
-}
-
-func validateEnv(env *actionlint.Env, msg string) error {
-	if env != nil && len(env.Vars) > 0 {
-		return fmt.Errorf("%s: %w", msg, errorDeclaredEnv)
-	}
-	return nil
+	return n
 }
 
 func formatActionlintError(errs []*actionlint.Error) error {
