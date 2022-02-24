3c9b24133b96fa391c3279b4b8a90b038df43824
Asra Ali <asraa@google.com>
2022-02-24 09:04:58 -0600

    make changes
    Signed-off-by: Asra Ali <asraa@google.com>

--- slsa-provenance/main.go
+++ slsa-provenance/main.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	cjson "github.com/docker/go/canonical/json"
 	intoto "github.com/in-toto/in-toto-golang/in_toto"
@@ -42,95 +43,85 @@ type GitHubContext struct {
 }
 
 func main() {
-	provenance, ok := os.LookupEnv("UNSIGNED_PROVENANCE")
 	// Generate the provenance
 	// TODO: Refactor with flags library.
+	digest, ok := os.LookupEnv("DIGEST")
 	if !ok {
-		digest, ok := os.LookupEnv("DIGEST")
-		if !ok {
-			panic(errors.New("Environment variable DIGEST not present"))
-		}
-
-		binary, ok := os.LookupEnv("UNTRUSTED_BINARY_NAME")
-		if !ok {
-			panic(errors.New("Environment variable UNTRUSTED_BINARY_NAME not present"))
-		}
-
-		githubContext, ok := os.LookupEnv("GITHUB_CONTEXT")
-		if !ok {
-			panic(errors.New("Environment variable GITHUB_CONTEXT not present"))
-		}
-
-		gh := &GitHubContext{}
-		if err := json.Unmarshal([]byte(githubContext), gh); err != nil {
-			panic(err)
-		}
-
-		if _, err := hex.DecodeString(digest); err != nil || len(digest) != 64 {
-			log.Fatal(fmt.Errorf("sha256 digest is not valid: %s", digest))
-		}
-
-		att := intoto.ProvenanceStatement{
-			StatementHeader: intoto.StatementHeader{
-				Type:          intoto.StatementInTotoV01,
-				PredicateType: slsa.PredicateSLSAProvenance,
-				Subject: []intoto.Subject{
-					{
-						Name: binary,
-						Digest: slsa.DigestSet{
-							"sha256": digest,
-						},
-					},
-				},
-			},
-			Predicate: slsa.ProvenancePredicate{
-				BuildType: "https://github.com/Attestations/GitHubActionsWorkflow@v1",
-				Builder: slsa.ProvenanceBuilder{
-					ID: "https://github.com/Attestations/GitHubHostedActions@v1",
-				},
-				Invocation: slsa.ProvenanceInvocation{
-					ConfigSource: slsa.ConfigSource{
-						EntryPoint: gh.Workflow,
-						URI:        fmt.Sprintf("git+%s.git", gh.Repository),
-						Digest: slsa.DigestSet{
-							"SHA1": gh.SHA,
-						},
-					},
-					// Add event inputs
-					Environment: map[string]interface{}{
-						"arch": "amd64", // TODO: Does GitHub run actually expose this?
-						"env": map[string]string{
-							"GITHUB_RUN_NUMBER": gh.RunNumber,
-							"GITHUB_RUN_ID":     gh.RunId,
-							"GITHUB_EVENT_NAME": gh.EventName,
-						},
-					},
-				},
-				Materials: []slsa.ProvenanceMaterial{{
-					URI: fmt.Sprintf("git+%s.git", gh.Repository),
-					Digest: slsa.DigestSet{
-						"SHA1": gh.SHA,
-					}},
-				},
-			},
-		}
-
-		attBytes, err := cjson.MarshalCanonical(att)
-		if err != nil {
-			panic(err)
-		}
-
-		filename := fmt.Sprintf("%s.intoto", binary)
-		if err := ioutil.WriteFile(filename, attBytes, 0600); err != nil {
-			panic(err)
-		}
-		fmt.Printf(`::set-output name=provenance-name::%s`, filename)
-		return
+		panic(errors.New("Environment variable DIGEST not present"))
 	}
 
-	attBytes, err := ioutil.ReadFile(provenance)
+	binary, ok := os.LookupEnv("UNTRUSTED_BINARY_NAME")
+	if !ok {
+		panic(errors.New("Environment variable UNTRUSTED_BINARY_NAME not present"))
+	}
+
+	// This binary filename is verified in the builder's dry-run step,
+	// but we verify it only contains alphanumeric characters again as well.
+	if err := verifyProvenanceName(binary); err != nil {
+		panic(err)
+	}
+
+	githubContext, ok := os.LookupEnv("GITHUB_CONTEXT")
+	if !ok {
+		panic(errors.New("Environment variable GITHUB_CONTEXT not present"))
+	}
+
+	gh := &GitHubContext{}
+	if err := json.Unmarshal([]byte(githubContext), gh); err != nil {
+		panic(err)
+	}
+
+	if _, err := hex.DecodeString(digest); err != nil || len(digest) != 64 {
+		log.Fatal(fmt.Errorf("sha256 digest is not valid: %s", digest))
+	}
+
+	att := intoto.ProvenanceStatement{
+		StatementHeader: intoto.StatementHeader{
+			Type:          intoto.StatementInTotoV01,
+			PredicateType: slsa.PredicateSLSAProvenance,
+			Subject: []intoto.Subject{
+				{
+					Name: binary,
+					Digest: slsa.DigestSet{
+						"sha256": digest,
+					},
+				},
+			},
+		},
+		Predicate: slsa.ProvenancePredicate{
+			BuildType: "https://github.com/Attestations/GitHubActionsWorkflow@v1",
+			Builder: slsa.ProvenanceBuilder{
+				ID: "https://github.com/Attestations/GitHubHostedActions@v1",
+			},
+			Invocation: slsa.ProvenanceInvocation{
+				ConfigSource: slsa.ConfigSource{
+					EntryPoint: gh.Workflow,
+					URI:        fmt.Sprintf("git+%s.git", gh.Repository),
+					Digest: slsa.DigestSet{
+						"SHA1": gh.SHA,
+					},
+				},
+				// Add event inputs
+				Environment: map[string]interface{}{
+					"arch": "amd64", // TODO: Does GitHub run actually expose this?
+					"env": map[string]string{
+						"GITHUB_RUN_NUMBER": gh.RunNumber,
+						"GITHUB_RUN_ID":     gh.RunId,
+						"GITHUB_EVENT_NAME": gh.EventName,
+					},
+				},
+			},
+			Materials: []slsa.ProvenanceMaterial{{
+				URI: fmt.Sprintf("git+%s.git", gh.Repository),
+				Digest: slsa.DigestSet{
+					"SHA1": gh.SHA,
+				}},
+			},
+		},
+	}
+
+	attBytes, err := cjson.MarshalCanonical(att)
 	if err != nil {
-		attBytes = []byte(provenance)
 		panic(err)
 	}
 
@@ -179,9 +170,25 @@ func main() {
 		panic(err)
 	}
 
-	filename := fmt.Sprintf("%s.sig", provenance)
+	filename := fmt.Sprintf("%s.intoto.sig", binary)
 	if err := ioutil.WriteFile(filename, payload, 0600); err != nil {
 		panic(err)
 	}
 	fmt.Printf(`::set-output name=signed-provenance-name::%s\n`, filename)
+}
+
+func verifyProvenanceName(name string) error {
+	const alpha = "abcdefghijklmnopqrstuvwxyz1234567890-_"
+
+	if name == "" {
+		return errors.New("empty provenance name")
+	}
+
+	for _, char := range name {
+		if !strings.Contains(alpha, strings.ToLower(string(char))) {
+			return fmt.Errorf("invalid filename: found character '%c' in %s", char, name)
+		}
+	}
+
+	return nil
 }
